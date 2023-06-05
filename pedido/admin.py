555a7ccf4faf6d1e84ee78879d19123b40f293ec
Julio <jncohenb@gmail.com>
2023-06-05 01:03:15 -0400

    Modelos Django Admin

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Producto, Pedido
 
-# Register your models here.
+admin.site.register(Producto)
+admin.site.register(Pedido)
--- pedido/models.py
+++ pedido/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/')
+
+    def __str__(self):
+        return self.nombre
+
+class Pedido(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    cantidad = models.PositiveIntegerField()
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"Pedido de {self.producto.nombre} - {self.fecha}"
